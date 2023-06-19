56013770d2753278a9f350c580200c92770ae3d3
praneu <prazolbikneup@gmail.com>
2023-06-19 14:58:29 -0400

    Angular-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Rob','rob@test.com', 5556669999, '','morning',true);
+  
 }
