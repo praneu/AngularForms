dfefd3eaf2756425aaa34a557d0407498e3c09cc
praneu <prazolbikneup@gmail.com>
2023-06-29 13:26:20 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('','rob@test.com', 5556669999, '','morning',true);
+  
+}
