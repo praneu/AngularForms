8fb1c08275f161fb2445b895721b77d7f53577de
praneu <prazolbikneup@gmail.com>
2023-06-29 13:49:50 -0400

    selectcontrol validated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,17 @@ import { User } from './user';
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
 
-  userModel = new User('','rob@test.com', 5556669999, '','morning',true);
+  topicHasError = true;
+
+  userModel = new User('Rob','rob@test.com', 5556669999, 'default','morning', true); 
+
+  validateTopic(value: string){
+     if(value == 'default'){
+      this.topicHasError= true;
+     }else {
+      this.topicHasError= false;
+     }
+
+  }
   
 }
